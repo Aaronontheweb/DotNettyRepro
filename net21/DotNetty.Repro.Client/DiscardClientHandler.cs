d35663e54a3bbc6526142371f53b0b931c0bc5b9
Aaron Stannard <aaron@petabridge.com>
2018-11-22 10:48:46 -0800

    created .NET Core 2.0 version and .NET Core 2.1 verison

---
+++ net21/DotNetty.Repro.Client/DiscardClientHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DotNetty.Buffers;
+using DotNetty.Transport.Channels;
+
+namespace DotNetty.Repro.Client
+{
+    public class DiscardClientHandler : SimpleChannelInboundHandler<object>
+    {
+        IChannelHandlerContext _ctx;
+        byte[] _array;
+
+        public override void ChannelActive(IChannelHandlerContext ctx)
+        {
+            this._array = new byte[12];
+            this._ctx = ctx;
+
+            Console.WriteLine("Connected to server");
+        }
+
+        protected override void ChannelRead0(IChannelHandlerContext context, object message)
+        {
+            // Server is supposed to send nothing, but if it sends something, discard it.
+        }
+
+        public override void ExceptionCaught(IChannelHandlerContext ctx, Exception e)
+        {
+            Console.WriteLine("{0}", e.ToString());
+            this._ctx.CloseAsync();
+        }
+    }
+}
---
+++ net21/DotNetty.Repro.Client/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using DotNetty.Handlers.Logging;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+
+namespace DotNetty.Repro.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up discard client");
+
+            var group = new MultithreadEventLoopGroup();
+
+
+            IPAddress host = null;
+
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DOTNETTY_REPRO_HOST")))
+            {
+                host = Dns.GetHostAddressesAsync(Environment.GetEnvironmentVariable("DOTNETTY_REPRO_HOST")).Result.FirstOrDefault();
+            }
+            else
+            {
+                host = IPAddress.Parse("127.0.0.1");
+            }
+
+            var bootstrap = new Bootstrap();
+            bootstrap
+                .Group(group)
+                .Channel<TcpSocketChannel>()
+                .Option(ChannelOption.TcpNodelay, true)
+                .Handler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                {
+                    IChannelPipeline pipeline = channel.Pipeline;
+
+                    pipeline.AddLast(new LoggingHandler());
+                    pipeline.AddLast(new DiscardClientHandler());
+                }));
+
+            IChannel bootstrapChannel = bootstrap.ConnectAsync(host, 1099).Result;
+
+            Console.CancelKeyPress += (sender, eventArgs) => bootstrapChannel.CloseAsync();
+
+            bootstrapChannel.CloseCompletion.Wait();
+            group.ShutdownGracefullyAsync().Wait(1000);
+        }
+    }
+}
